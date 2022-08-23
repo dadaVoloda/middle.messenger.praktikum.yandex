f3b3e1d3326f0fea3f6c3ccbdf90f07a49db2e72
Vladimir Che <vladimirchukarev@gmail.com>
2022-08-23 21:28:14 +0300

    fix navigation

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const fallback = require('express-history-api-fallback')
 const express = require('express')
 
 const app = express()
+const root = __dirname + '/dist'
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('./dist/'))
+app.use(express.static(root))
+app.use(fallback('index.html', { root }))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
