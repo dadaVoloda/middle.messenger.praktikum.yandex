2426009d6b990002bd95006c19697f5ab0020341
Vladimir Che <vladimirchukarev@gmail.com>
2022-08-23 07:52:49 +0300

    add server

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,15 +1,15 @@
 import search from './search.hbs'
 import './search.scss'
-import toggleActive from '../../utils/toggleActiveInputText'
+// import toggleActive from '../../utils/toggleActiveInputText'
 
 document.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('.search__input')
 
-  if (input) {
-    input.addEventListener('input', (e) =>
-      toggleActive(e, '.search', '.search__label-text-wrapper')
-    )
-  }
+  // if (input) {
+  //   input.addEventListener('input', (e) =>
+  //     toggleActive(e, '.search', '.search__label-text-wrapper')
+  //   )
+  // }
 })
 
 export default () => search()
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,34 +1,36 @@
-import tpl from './login.hbs';
-import loginForm from '../../components/login-form';
-import button from '../../components/button';
-import field from '../../components/field';
-import toggleActive from '../../utils/toggleActiveInputText';
+import tpl from './login.hbs'
+import loginForm from '../../components/login-form'
+import button from '../../components/button'
+import field from '../../components/field'
+// import toggleActive from '../../utils/toggleActiveInputText'
 
 const data = {
   loginForm: loginForm(
     'Вход',
     [
       {
-        field: field('text', 'login', 'true', 'Логин')
+        field: field('text', 'login', 'true', 'Логин'),
       },
       {
-        field: field('password', 'password', 'false', 'Пароль')
-      }
+        field: field('password', 'password', 'false', 'Пароль'),
+      },
     ],
     button('login-form__btn', 'submit', 'Авторизоваться'),
     {
       path: '/signup',
-      text: 'Нет аккаунта?'
+      text: 'Нет аккаунта?',
     }
-  )
-};
+  ),
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const inputs = document.querySelectorAll('.field__input');
-  
-  [...inputs].forEach((inp) => {
-    inp.addEventListener('input', (e) => toggleActive(e, '.field', '.field__label-text'));
-  });
-});
+  const inputs = document.querySelectorAll('.field__input')
 
-export default tpl(data);
+  // if (inputs) {
+  //   inputs.forEach((inp) => {
+  //     inp.addEventListener('input', (e) => toggleActive(e, '.field', '.field__label-text'))
+  //   })
+  // }
+})
+
+export default tpl(data)
